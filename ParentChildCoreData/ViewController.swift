04d8f4818c920ff9f419db589f1d75fd81c76196
Soham Bhattacharjee <sohamb.1390@gmail.com>
2016-07-03 14:32:26 +0530

    1.

--- ParentChildCoreData/ViewController.swift
+++ ParentChildCoreData/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         self.privateMOC = self.appDelegate.createPrivateMOC()
         self.mainMOC = self.appDelegate.mainMOC
         self.writerMOC = self.appDelegate.masterMOC
@@ -31,11 +31,43 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        self.saveToPrivateContext()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Fetch Products
+    func fetchProducts(moc: NSManagedObjectContext!) {
+        // Initialize Fetch Request
+        let fetchRequest = NSFetchRequest()
         
+        // Create Entity Description
+        let entityDescription = NSEntityDescription.entityForName("Product", inManagedObjectContext: moc)
+        
+        // Configure Fetch Request
+        fetchRequest.entity = entityDescription
+        let sortDesc = NSSortDescriptor(key: "pName", ascending: true)
+        fetchRequest.sortDescriptors = [sortDesc]
+        fetchRequest.fetchLimit = 100
+        do {
+            if let resultArray:[Product] = try self.mainMOC!.executeFetchRequest(fetchRequest) as? [Product] {
+                self.arrProducts = resultArray
+            }
+        } catch {
+            let fetchError = error as NSError
+            print(fetchError)
+        }
+    }
+    
+    // MARK: - Writing data -> Private Context -> Main Context -> Writer Context
+    func saveToPrivateContext() {
+        // Checking whether the PrivateManagedObjectContext is nil or not
         if self.privateMOC != nil {
             
-            self.privateMOC?.performBlock({ 
-                for index in 0..<5000 {
+            self.privateMOC?.performBlock({
+                for index in 0..<20000 {
                     if let newProduct: Product = NSEntityDescription.insertNewObjectForEntityForName("Product", inManagedObjectContext: self.privateMOC!) as? Product {
                         newProduct.pName = "P\(index + 1)"
                         newProduct.pPrice = "\(index + 1)"
@@ -46,7 +78,7 @@ class ViewController: UIViewController {
                             
                             // This is optional, I am putting the privateQueue in sleep for 5 seconds for better performance
                             if index%1000 == 0 {
-                                sleep(5)
+                                sleep(2)
                             }
                         }
                         catch let error as NSError {
@@ -54,66 +86,53 @@ class ViewController: UIViewController {
                         }
                     }
                 }
+                // Final save operation on Private ManagedObjectContext
                 do {
                     try self.privateMOC!.save()
                 }
                 catch let error as NSError {
                     print(error.localizedDescription)
                 }
+                // Fetching products using the private context
+                self.fetchProducts(self.privateMOC)
                 
-                if self.mainMOC != nil {
-                    self.mainMOC?.performBlock({ 
-                        // Initialize Fetch Request
-                        let fetchRequest = NSFetchRequest()
-                        
-                        // Create Entity Description
-                        let entityDescription = NSEntityDescription.entityForName("Product", inManagedObjectContext: self.mainMOC!)
-                        
-                        // Configure Fetch Request
-                        fetchRequest.entity = entityDescription
-                        let sortDesc = NSSortDescriptor(key: "pName", ascending: true)
-                        fetchRequest.sortDescriptors = [sortDesc]
-                        
+                // Saving content on Main ManagedObjectContext & Writer ManagedObjectContext
+                self.saveToParentAndWriterContext()
+            })
+        }
+    }
+    
+    func saveToParentAndWriterContext() {
+        if self.mainMOC != nil {
+            self.mainMOC?.performBlock({
+                
+                // Updating UI
+                self.tableView.reloadData()
+                
+                do {
+                    try self.mainMOC!.save()
+                }
+                catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+                
+                /// Finally the writer context will perform the save opration and persist the data to the disk
+                if self.writerMOC != nil {
+                    self.writerMOC?.performBlock({
                         do {
-                            if let resultArray:[Product] = try self.mainMOC!.executeFetchRequest(fetchRequest) as? [Product] {
-                                self.arrProducts = resultArray
-                                print(self.arrProducts)
-                                self.tableView.reloadData()
-                            }
-                            
-                        } catch {
-                            let fetchError = error as NSError
-                            print(fetchError)
-                        }
-                        
-                        do {
-                            try self.mainMOC!.save()
+                            try self.writerMOC!.save()
                         }
                         catch let error as NSError {
                             print(error.localizedDescription)
-                        }
-                        
-                        if self.writerMOC != nil {
-                            self.writerMOC?.performBlock({ 
-                                do {
-                                    try self.writerMOC!.save()
-                                }
-                                catch let error as NSError {
-                                    print(error.localizedDescription)
-                                }
-                            })
                         }
                     })
                 }
             })
         }
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
+// MARK: - UITableView DataSource & Delegates
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
